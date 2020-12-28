a5f0b187022f5a822dbc1ec29c7f6d6f10d40416
Aglailson <jaglailson1@gmail.com>
2020-12-28 15:19:59 -0300

    slide finalizado

--- js/main.js
+++ js/main.js
@@ -5,6 +5,126 @@ window.onload =  function(){
 	const destaques = new Destaques()
 	destaques.carregaItens(content_destaques)
 
+var slider = document.getElementById('slider')
+var sliderItems = document.getElementById('slides')
+var prev = document.getElementById('btn-prev')
+var next = document.getElementById('btn-next')
+
+function slide(wrapper, items, prev, next){
+	let posX1 = 0
+	let posX2 = 0
+	let posInicial
+	let posFinal
+	let threshold = 100
+	let slides = items.getElementsByClassName('slide')
+	let slidesLength = slides.length
+	let slideSize = items.getElementsByClassName('slide')[0].offsetWidth
+	let firstSlide = slides[0]
+	let lastSlide = slides[slidesLength - 1]
+	let cloneFirst = firstSlide.cloneNode(true)
+	let cloneLast = lastSlide.cloneNode(true)
+	let index = 0
+	let allowShift = true
+
+
+	//Clona o primeiro e ultimo slide
+	items.appendChild(cloneFirst)
+	items.insertBefore(cloneLast, firstSlide)
+	wrapper.classList.add('loaded')
+
+	//Eventos do mouse
+	items.onmousedown = dragStart;
+
+	//Evento de toque
+	items.addEventListener('touchstart', dragStart)
+	items.addEventListener('touchend', dragEnd)
+	items.addEventListener('touchmov', dragAction)
+
+	//Eventos de clique
+	prev.addEventListener('click', function(){ shiftSlide(-1)})
+	next.addEventListener('click', function(){ shiftSlide(1)})
+
+	//Eventos de transição
+	items.addEventListener('transitionend', checkIndex)
+
+	function dragStart(e){
+		e = e || window.event
+		e.preventDefault()
+		posInicial = items.offsetLeft;
+
+		if(e.type == 'touchstart'){
+			posX1 = e.touches[0].clientX
+		}else{
+			posX1 = e.clientX
+			document.onmouseup = dragEnd
+			document.onmousemove = dragAction
+		}
+	}
+
+	function dragAction(e){
+		e = e || window.event
+
+		if(e.type == 'touchmove'){
+			posX2 = posX1 - e.touches[0].clientX
+			posX1 = e.touches[0].clientX
+		}else{
+			posX2 = posX1 - e.clientX
+			posX1 = e.clientX
+		}
+		items.style.left = (items.offsetLeft - posX2 +"px")
+	}
+
+	function dragEnd(e){
+		posFinal = items.offsetLeft
+
+		if(posFinal - posInicial < -threshold){
+			shiftSlide(1, 'drag')
+		}else if(posFinal - posInicial > threshold){
+			shiftSlide(-1, 'drag')
+		}else{
+			items.style.left = (posInicial)+"px"
+		}
+
+		document.onmouseup = null
+		document.onmousemove = null
+	}
+
+	function shiftSlide(dir, action){
+		items.classList.add('shifting')
+
+		if(allowShift){
+			if(!action){posInicial = items.offsetLeft}
+
+			if(dir == 1){
+				items.style.left = (posInicial - slideSize) + "px"
+				index++
+			}else if(dir == -1){
+				items.style.left = (posInicial + slideSize) + "px"
+				index--
+			}
+		};
+		allowShift = false
+	}
+
+	function checkIndex(){
+		items.classList.remove('shifting')
+
+		if(index == -1){
+			items.style.left = -(slidesLength * slideSize) + "px"
+			index = slidesLength - 1
+		}
+
+		if(index == slidesLength){
+			items.style.left = - (1 * slideSize) + "px"
+			index = 0
+		}
+
+		allowShift = true
+	}
+}
+
+slide(slider, sliderItems,prev, next)
+	
 
 }
 
@@ -285,12 +405,11 @@ function Destaques(){
 		})
 
 	}
-
 }
 
 
-function Slider(){
 
-	this.itens = []
+/*slider*/
 
-}
+
+
