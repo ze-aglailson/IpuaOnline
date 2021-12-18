6b57e877771a0c542c5994a9c33d2fed94fb3741
zeaglailson <jaglailson1@gmail.com>
2021-12-18 19:42:01 -0300

    Organizaçao configs sliders

---
+++ assets/js/slider.js
@@ -0,0 +1,13 @@
+/* Arquivo de configuração dos sliders */
+$(document).ready(function(){
+    
+    $("#slider-principal").owlCarousel({
+        items:1,
+        loop:true,
+        dots:false,
+        autoplay:true,
+        autoplayTimeout:4500,
+
+    });
+
+})
